27b313cef9a2ff4b11419b61c62306794c832b96
subhranshu khatua <devastra093@gmail.com>
2026-04-16 01:11:26 +0530

    hard fix contact module bug

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,4 @@
-"use client";
-
-export default function ContactPage() {
+export default function Page() {
   return (
     <main className="min-h-screen flex items-center justify-center text-white">
       <h1 className="text-3xl font-bold">
