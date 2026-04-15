e2751d1dde455101b8a872f8ab675fcb18181116
subhranshu khatua <devastra093@gmail.com>
2026-04-16 01:50:28 +0530

    fix contact module issue clean

--- src/app/contact/page.jsx
+++
@@ -1,7 +0,0 @@
-export default function ContactPage() {
-  return (
-    <main className="min-h-screen flex items-center justify-center text-white">
-      <h1>Contact Page 🚀</h1>
-    </main>
-  );
-}
