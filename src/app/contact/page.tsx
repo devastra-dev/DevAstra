7589a725eda66075eec28cb00d6f468f59a6a8c7
subhranshu khatua <devastra093@gmail.com>
2026-04-16 00:43:57 +0530

    fix contact page module error

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function ContactPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center text-white">
+      <h1 className="text-3xl font-bold">
+        Contact Page 🚀
+      </h1>
+    </main>
+  );
+}
