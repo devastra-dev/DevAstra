1c05006ec7f95a610340534a92eab74254336a87
subhranshu khatua <devastra093@gmail.com>
2026-04-16 01:44:44 +0530

    fix contact page TS issue via jsx

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,7 @@
+export default function ContactPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center text-white">
+      <h1>Contact Page 🚀</h1>
+    </main>
+  );
+}
