9842d7c53de6ec2d28867173f3808e57e9f5765a
subhranshu khatua <devastra093@gmail.com>
2026-04-16 01:26:42 +0530

    fix eslint react-hooks error

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,3 @@
 export default function Page() {
-  return (
-    <main className="min-h-screen flex items-center justify-center text-white">
-      <h1 className="text-3xl font-bold">
-        Contact Page 🚀
-      </h1>
-    </main>
-  );
+  return <div>Contact Page</div>;
 }
